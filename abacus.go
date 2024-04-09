6e6df7a0b98dc0f76ff1c8658e013cd51593fda1
dmytromykhailenko <daemonkt@gmail.com>
2024-04-09 10:29:50 +0300

    Day 2. Task 1

--- abacus.go
+++ abacus.go
@@ -1,21 +1,4 @@
-package main
-
-import (
-	"fmt"
-	"os"
-	"strconv"
-)
-
-func main() {
-	num1 := os.Args[1]
-	num2 := os.Args[2]
-
-	a, _ := strconv.Atoi(num1)
-	b, _ := strconv.Atoi(num2)
-
-	fmt.Println(Abacus(a, b))
-
-}
+package sprint
 
 func Abacus(a int, b int) int {
 
