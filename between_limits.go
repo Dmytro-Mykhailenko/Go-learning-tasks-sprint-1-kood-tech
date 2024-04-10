3d6576e9c27776e5b624e020c1e913dbbde1e2c5
dmytromykhailenko <daemonkt@gmail.com>
2024-04-10 13:22:24 +0300

    Day 2. Task 5

--- between_limits.go
+++ between_limits.go
@@ -3,13 +3,25 @@ package sprint
 func BetweenLimits(from, to rune) string {
 
 	var finalStr string
-	from += 1
 
-	for from < to {
-
-		finalStr += string(from)
+	if from < to {
 		from += 1
+		for from < to {
 
+			finalStr += string(from)
+			from += 1
+
+		}
+	} else {
+
+		to += 1
+		for to < from {
+
+			finalStr += string(to)
+			to += 1
+
+		}
 	}
+
 	return finalStr
 }
