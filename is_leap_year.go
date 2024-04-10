85cbc383660b0ac4cc8eef6784a77e93695b4ee0
dmytromykhailenko <daemonkt@gmail.com>
2024-04-10 12:37:01 +0300

    Day 2. Task 7

--- is_leap_year.go
+++ is_leap_year.go
@@ -2,11 +2,12 @@ package sprint
 
 func IsLeapYear(year int) bool {
 
-	if year%400 != 0 {
-		return false
-	} else if year%4 == 0 {
+	if year%4 == 0 {
+		if year%100 == 0 && year%400 != 0 {
+			return false
+		}
+
 		return true
-	} else {
-		return false
 	}
+	return false
 }
