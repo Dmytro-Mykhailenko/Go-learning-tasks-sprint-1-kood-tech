8b1fb8855de084098e76dc9153791797e7cc90c8
dmytromykhailenko <daemonkt@gmail.com>
2024-04-11 10:11:27 +0300

    Day 4. Task 2

---
+++ reverse_alphabet.go
@@ -0,0 +1,25 @@
+package sprint
+
+func ReverseAlphabet(step int) string {
+
+	if step <= 0 {
+
+		step = 1
+
+	}
+
+	ch := 'z'
+
+	var resStr string
+
+	for ch >= 'a' {
+
+		resStr += string(ch)
+
+		ch += rune(step)
+
+	}
+
+	return resStr
+
+}
