dfd453af2273805ebf29dc96d2e3f723cfbe9935
dmytromykhailenko <daemonkt@gmail.com>
2024-04-15 10:10:27 +0300

    Day 6. Task 2

---
+++ remove_elements_in_range.go
@@ -0,0 +1,28 @@
+package sprint
+
+func RemoveElementsInRange(arr []float64, from, to int) []float64 {
+
+	var minI, maxI int
+
+	if (from < 0 && to < 0) || (from >= len(arr) && to >= len(arr)) {
+		return arr
+	} else if from <= to {
+		if to < 0 {
+			minI = 0
+		} else {
+			minI = from
+		}
+		maxI = to
+	} else if from >= to {
+		if to < 0 {
+			minI = 0
+		} else {
+			minI = to
+		}
+		maxI = from
+	}
+
+	arr = append(arr[:minI], arr[maxI:]...)
+
+	return arr
+}
