8e1850166cc811fa35cd1ba0ce50d48e3b3e4c9d
dmytromykhailenko <daemonkt@gmail.com>
2024-04-15 10:46:47 +0300

    Day 6. Task 2

--- remove_elements_in_range.go
+++ remove_elements_in_range.go
@@ -6,33 +6,23 @@ func RemoveElementsInRange(arr []float64, from, to int) []float64 {
 
 	// if (from < 0 && to < 0) || (from >= len(arr) && to >= len(arr)) {
 	// 	return arr
-	if from < to {
-		if f < 0 {
+	if (from < 0 && to < 0) || (from >= len(arr) && to >= len(arr)) {
+		return arr
+	} else if from <= to {
+		if from < 0 {
 			minI = 0
 		} else {
 			minI = from
 		}
-		if to > len(arr) {
-			maxI = len(arr)
-		} else {
-			maxI = to
-		}
-	}
-
-	if from > to {
+		maxI = to
+	} else if from >= to {
 		if to < 0 {
 			minI = 0
 		} else {
 			minI = to
 		}
-
-		if from > len(arr) {
-			maxI = len(arr)
-		} else {
-			maxI = from
-		}
+		maxI = from
 	}
-
 	arr = append(arr[:minI], arr[maxI:]...)
 
 	return arr
