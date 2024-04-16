559c639c919269a592a65ad5a750872104f2b4e2
dmytromykhailenko <daemonkt@gmail.com>
2024-04-16 22:08:28 +0300

    Day 7. Task 13

--- substr_index.go
+++ substr_index.go
@@ -2,7 +2,13 @@ package sprint
 
 func SubstrIndex(s string, toFind string) int {
 
+	if len(toFind) == 0 || toFind == s {
+
+		return 0
+	}
+
 	cnt := 0
+
 	for i, _ := range s {
 		if i+len(toFind) <= len(s) {
 			if s[i:i+len(toFind)] == toFind {
@@ -10,8 +16,10 @@ func SubstrIndex(s string, toFind string) int {
 			}
 		}
 	}
+
 	if cnt == 0 {
 		return -1
 	}
+
 	return cnt
 }
