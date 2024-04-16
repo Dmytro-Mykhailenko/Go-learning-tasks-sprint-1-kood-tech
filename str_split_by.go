f5e9153431621a7b8a761dad0c0c613014340079
dmytromykhailenko <daemonkt@gmail.com>
2024-04-16 17:16:59 +0300

    Day 7. Task 12

--- str_split_by.go
+++ str_split_by.go
@@ -1,6 +1,41 @@
 package sprint
 
 func StrSplitBy(s, sep string) []string {
+	res := []string{}
+	var word, spr []byte
 
-	return []string{s}
+	var IsSep bool
+	cnt := 0
+
+	for i := 0; i < len(s); i++ {
+
+		if s[i] == sep[cnt] {
+			if cnt == len(sep)-1 {
+				IsSep = true
+				spr = []byte{}
+			} else {
+				spr = append(spr, s[i])
+				cnt++
+				continue
+			}
+		} else {
+			word = append(spr, word...)
+			spr = []byte{}
+			cnt = 0
+		}
+
+		if IsSep {
+			res = append(res, string(spr)+string(word))
+			word = []byte{}
+			IsSep = false
+			cnt = 0
+			continue
+		}
+
+		word = append(word, s[i])
+	}
+
+	res = append(res, string(word)+string(spr))
+
+	return res
 }
