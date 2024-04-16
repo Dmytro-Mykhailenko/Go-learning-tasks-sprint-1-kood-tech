343a78bc0fa2a7cb0d48cf5c45910f8f3601b1db
dmytromykhailenko <daemonkt@gmail.com>
2024-04-16 11:54:22 +0300

    Day 7. Task 8

--- to_upper_case.go
+++ to_upper_case.go
@@ -1,15 +1,11 @@
 package sprint
 
-import "unicode"
-
 func ToUpperCase(s string) string {
 	var str string
 	for _, ch := range s {
-		if unicode.IsLetter(ch) {
-			if unicode.IsLower(ch) {
-				str += string(ch - 32)
-				continue
-			}
+		if IsLower(string(ch)) {
+			str += string(ch - 32)
+			continue
 		}
 		str += string(ch)
 	}
