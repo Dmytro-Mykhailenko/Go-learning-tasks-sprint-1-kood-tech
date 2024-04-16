ff0f20e59ffd8f3ecf924cacd4dbee3abc702cd3
dmytromykhailenko <daemonkt@gmail.com>
2024-04-16 11:57:50 +0300

    Day 7. Task 8

--- to_upper_case.go
+++ to_upper_case.go
@@ -3,11 +3,18 @@ package sprint
 func ToUpperCase(s string) string {
 	var str string
 	for _, ch := range s {
-		if IsLower(string(ch)) {
+		if IsLower(ch) {
 			str += string(ch - 32)
 			continue
 		}
 		str += string(ch)
 	}
 	return str
+}
+
+func IsLower(ch rune) bool {
+	if ch >= 'a' && ch <= 'z' {
+		return true
+	}
+	return false
 }
