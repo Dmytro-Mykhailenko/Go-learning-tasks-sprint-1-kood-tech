2b0d71a595478d10fb185112a946e70cfa1625ca
dmytromykhailenko <daemonkt@gmail.com>
2024-04-16 23:27:21 +0300

    Day 7. Task 13

--- substr_index.go
+++ substr_index.go
@@ -9,11 +9,9 @@ func SubstrIndex(s string, toFind string) int {
 
 	cnt := 0
 
-	for i := 0; i < len(s); i++ {
-		if i+len(toFind) <= len(s) {
-			if s[i:i+len(toFind)] == toFind {
-				cnt++
-			}
+	for i := 0; i <= len(s)-len(toFind); i++ {
+		if s[i:i+len(toFind)] == toFind {
+			cnt++
 		}
 	}
 
