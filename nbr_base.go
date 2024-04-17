28f884835fd854352ccbcaa99a273beffcf764ad
dmytromykhailenko <daemonkt@gmail.com>
2024-04-17 13:36:04 +0300

    Day 7. Task 15

--- nbr_base.go
+++ nbr_base.go
@@ -2,6 +2,40 @@ package sprint
 
 func NbrBase(n int, base string) string {
 
+	bs := []rune(base)
+
+	if len(bs) <= 2 {
+		if len(bs) < 2 {
+			return "NV"
+		} else {
+			if bs[0] == bs[1] {
+				return "NV"
+			}
+		}
+	}
+
+	for i := 0; i < len(bs); i++ {
+		cnt := 0
+
+		for j := i + 1; j < len(bs); j++ {
+			if bs[i] == '+' || bs[j] == '+' || bs[i] == '-' || bs[j] == '-' {
+				return "NV"
+			}
+
+			if bs[i] == bs[j] {
+				cnt++
+				continue
+			}
+		}
+
+		if cnt == len(bs)-1 {
+			return "NV"
+		}
+	}
+	return Itoa(n, base)
+}
+
+func Itoa(n int, base string) string {
 	var resStr string
 	var isNegative bool
 
