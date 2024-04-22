6cf2e34d9bdb80fe052fba292739025b94bbb555
dmytromykhailenko <daemonkt@gmail.com>
2024-04-22 23:31:15 +0300

    Week 3. Task 8

--- sqrt.go
+++ sqrt.go
@@ -22,3 +22,19 @@ func Sqrt(n int) int {
 	}
 	return 0
 }
+
+func ToThePowerIterative(n int, power int) int {
+
+	if power < 0 {
+		return 0
+	}
+
+	x := 1
+
+	for i := 0; i < power; i++ {
+
+		x *= n
+	}
+
+	return x
+}
