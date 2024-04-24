648d9f60eb9431cfae52995b665c772310c010f2
dmytromykhailenko <daemonkt@gmail.com>
2024-04-25 00:12:19 +0300

    Week 3. Task 24

--- from_roman.go
+++ from_roman.go
@@ -3,32 +3,57 @@ package sprint
 func FromRoman(s string) int {
 
 	var x int
+	var res int
 
-	for _, ch := range s {
+	for x < len(s) {
 
-		switch ch {
+		if x < len(s)-1 {
 
-		case 'M':
-			x += 1000
+			if retRomNum(rune(s[x])) < retRomNum(rune(s[x+1])) {
 
-		case 'D':
-			x += 500
+				res -= retRomNum(rune(s[x]))
+			} else {
 
-		case 'C':
-			x += 100
+				res += retRomNum(rune(s[x]))
+			}
 
-		case 'L':
-			x += 50
+		} else {
 
-		case 'X':
-			x += 10
-
-		case 'V':
-			x += 5
-
-		case 'I':
-			x += 1
+			res += retRomNum(rune(s[x]))
 		}
+
+		x++
+	}
+
+	return res
+}
+
+func retRomNum(ch rune) int {
+
+	var x int
+
+	switch ch {
+
+	case 'M':
+		x += 1000
+
+	case 'D':
+		x += 500
+
+	case 'C':
+		x += 100
+
+	case 'L':
+		x += 50
+
+	case 'X':
+		x += 10
+
+	case 'V':
+		x += 5
+
+	case 'I':
+		x += 1
 	}
 
 	return x
